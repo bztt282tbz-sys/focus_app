31ffbc990385ea8e257122fc7c9547a2a572aebc
Dennis <dennis@email.gov>
2026-04-27 12:30:29 +0200

    about page

--- app.py
+++ app.py
@@ -128,6 +128,10 @@ def onboarding_required(f):
 def index():
     return render_template('home.html', title="Home")
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
+
 @app.route('/onboarding', methods=['GET', 'POST'])
 @login_required
 def onboarding():
